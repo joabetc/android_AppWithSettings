daa6c6bf645cfc7c8b56a68662a1980e3f5458e8
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-12-03 16:57:16 -0300

    Adding an Activity for settings

---
+++ app/src/main/java/com/joabe/appwithsettings/SettingsActivity.java
@@ -0,0 +1,13 @@
+package com.joabe.appwithsettings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
